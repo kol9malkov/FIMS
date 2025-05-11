248fd3525833b595f37519f20f110c65b7ccd414
kol9malkov <kol9malkov@gmail.com>
2025-05-12 01:17:03 +0300

    Обновил логику crud/category и ручки

--- routes/store/category.py
+++ routes/store/category.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from dependencies.dependence import admin_required, store_required
 from schemas import CategoryCreate, CategoryUpdate, CategoryResponse
 from crud import category as crud_category
 from utils import get_db
@@ -7,8 +9,11 @@ from utils import get_db
 router = APIRouter()
 
 
-@router.post("/create", response_model=CategoryResponse)
+@router.post("/create", response_model=CategoryResponse, dependencies=[Depends(admin_required)])
 def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
+    db_category = crud_category.get_category_by_name(db, category.name)
+    if db_category:
+        raise HTTPException(status_code=409, detail="Категория уже существует")
     return crud_category.create_category(db, category)
 
 
@@ -17,19 +22,47 @@ def get_categories(db: Session = Depends(get_db)):
     return crud_category.get_all_categories(db)
 
 
-@router.get("/{category_id}", response_model=CategoryResponse)
-def get_category(category_id: int, db: Session = Depends(get_db)):
+@router.get("/id/{category_id}", response_model=CategoryResponse)
+def get_category_by_id(category_id: int, db: Session = Depends(get_db)):
     db_category = crud_category.get_category_by_id(db, category_id)
     if not db_category:
-        raise HTTPException(status_code=404, detail="Category not found")
+        raise HTTPException(status_code=404, detail="Категория не найдена")
     return db_category
 
 
-@router.put("/{category_id}", response_model=CategoryResponse)
-def update_category(category_id: int, category: CategoryUpdate, db: Session = Depends(get_db)):
-    return crud_category.update_category(db, category_id, category)
+@router.get("/by_name/{category_name}", response_model=CategoryResponse)
+def get_category_by_name(category_name: str, db: Session = Depends(get_db)):
+    db_category = crud_category.get_category_by_name(db, category_name)
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Категория не найдена")
+    return db_category
 
 
-@router.delete("/{category_id}", response_model=CategoryResponse)
+@router.put("/by-name/{category_name}", response_model=CategoryResponse, dependencies=[Depends(admin_required)])
+def update_category(category_name: str, category: CategoryUpdate, db: Session = Depends(get_db)):
+    db_category = crud_category.get_category_by_name(db, category_name)
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Категория не найдена")
+    return crud_category.update_category(db, db_category, category)
+
+
+@router.put("/id/{category_id}", response_model=CategoryResponse, dependencies=[Depends(admin_required)])
+def update_category_by_id(category_id: int, category: CategoryUpdate, db: Session = Depends(get_db)):
+    db_category = crud_category.get_category_by_id(db, category_id)
+    if not db_category:
+        raise HTTPException(status_code=404, detail="Категория не найдена")
+    return crud_category.update_category(db, db_category, category)
+
+
+@router.delete("/id/{category_id}", status_code=204, dependencies=[Depends(admin_required)])
 def delete_category(category_id: int, db: Session = Depends(get_db)):
-    return crud_category.delete_category(db, category_id)
+    if not crud_category.delete_category(db, category_id):
+        raise HTTPException(status_code=404, detail="Категория не найдена")
+
+
+@router.delete("/by-name/{category_name}", status_code=204, dependencies=[Depends(admin_required)])
+def delete_category_by_name(category_name: str, db: Session = Depends(get_db)):
+    category = crud_category.get_category_by_name(db, category_name)
+    if not category:
+        raise HTTPException(status_code=404, detail="Категория не найдена")
+    crud_category.delete_category(db, category.category_id)
